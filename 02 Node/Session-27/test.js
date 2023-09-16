8346e6425762ee0964b66d31c248b6abbfacba27
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-09-16 20:45:58 +0530

    Node Notes

---
+++ 02 Node/Session-27/test.js
@@ -0,0 +1,13 @@
+console.log("Custom File");
+
+function hello(){
+    console.log("Hello World");
+}
+
+hello();
+
+const array=[1,2,3,4,5];
+
+array.forEach((value)=>{
+    console.log(value);
+})
