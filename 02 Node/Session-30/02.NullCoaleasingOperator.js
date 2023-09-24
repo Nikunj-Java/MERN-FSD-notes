2ff9e71771690bbb1ea0d82f78ae392d9689874d
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-09-24 20:43:19 +0530

    Enhance Object Literals,NullCoaleasingOperator

---
+++ 02 Node/Session-30/02.NullCoaleasingOperator.js
@@ -0,0 +1,14 @@
+function check(value){
+    const defaultValue='Default';
+
+    const result=value ?? defaultValue;
+
+    return result;
+}
+
+console.log(check('Hello'));
+console.log(check(null));
+console.log(check(undefined));
+console.log("Result",check(""));
+console.log("Result",check(0));
+console.log("Result",check(false));
