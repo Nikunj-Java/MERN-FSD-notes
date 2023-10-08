bd8cbdabf342a0594a57813b719f8a4150f630b3
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-10-08 20:38:05 +0530

    Cookie Live Demo,REACT APP

---
+++ 02 Node/Session-34/firstapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>My First React APP</h1>
+    </div>
+  );
+}
+
+export default App;
