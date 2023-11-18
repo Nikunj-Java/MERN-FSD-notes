0bff2bbbd4db3404ff11f757850890c599e0294f
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-11-18 20:31:20 +0530

    Happy Diwali

--- 03 React/Session-41/diwalicontextapp/src/components/usecase2/MyChild.js
+++ 03 React/Session-41/diwalicontextapp/src/components/usecase2/MyChild.js
@@ -1,12 +1,12 @@
 import { useMyContext } from "./MyContext";
 
 function MyChild(){
-    const {data,SetData}=useMyContext();
+    const {data,setData}=useMyContext();
 
     return (<div>
         <h3> Child</h3>
         <p>{data}</p>
-        <input type='text' value={data} onChange={(e)=>SetData(e.target.value)}/>
+        <input type='text' value={data} onChange={(e)=>setData(e.target.value)}/>
     </div>)
 }
 
