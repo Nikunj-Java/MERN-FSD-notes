d5694517d181e297d63357af2f99a516fa6b0425
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-23 20:36:51 +0530

    Node RestAPI

--- BackUp Session-1/server.js
+++ BackUp Session-1/server.js
@@ -20,6 +20,24 @@ const mytodos= [
 app.get('/api/mytodos',(req,res)=>{
     res.json(mytodos);
 });
+
+//get todos by id
+app.get('/api/mytodos/:id',(req,res)=>{
+
+    //parseint is used to convert anyvalue to IntegerValue
+    //if parseInt(String)===> string is converted forcefully to  IntegerValue
+   const id= parseInt(req.params.id);
+   const mytodo=mytodos.find( mytodo=>mytodo.id===id)
+
+   if(!mytodo){
+    res.status(404).json({error: 'No Todo Found'});
+    return;
+   }
+    res.json(mytodo);
+});
+
+
+
 //start the server to particular port
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`);
@@ -27,3 +45,9 @@ app.listen(port,()=>{
  
 //try to fetch data using id;
 //localhost:3000/api/mytodos/1
+
+
+//notes: 
+//1. install express: npm install express
+//2. start the server: node server.js
+//3. localhost:3000/api/mytodos
