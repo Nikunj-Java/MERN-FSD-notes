20f9b6da07130b0d984dbf3ad9903b9a9b662587
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-02-18 21:11:36 +0530

    File Uploads,Download

--- 05 Node/Session-59/routes/product.routes.js
+++ 05 Node/Session-59/routes/product.routes.js
@@ -14,5 +14,9 @@ router.post('/',upload.single('picture'),addProduct);
 router.get('/',getAllProducts);
 router.get('/:id',getProductById);
 
+//to get the image
+//http://localhost:portno/foldername/imagename.png
+//eg: http://localhost:5000/uploads/rate-1708267337413-0.4902616183566526.png
+
 
 module.exports=router;
