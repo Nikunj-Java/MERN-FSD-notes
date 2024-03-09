7f4a83bf805f42058d8d3aac878b365a716a2fc0
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-03-09 21:32:44 +0530

    Capstone Frontend and Backend

--- 06 Capstone/Session 61/backend/server.js
+++ 06 Capstone/Session 61/backend/server.js
@@ -29,6 +29,32 @@ app.post('/signup',async (req,res)=>{
     }
 })
 
+app.post('/signup',async (req,res)=>{
+    const {username,password}=req.body;
+    try {
+        
+        const newUser=new User({username,password});
+        await newUser.save();
+        res.json({success:true,message:'User Registered Successfully'})
+    } catch (error) {
+        console.error("Error Signing up:",error.message);
+        res.json({success:false,message:'Error While Sihning up'})
+    }
+});
+app.post('/login',async (req,res)=>{
+    const {username,password}=req.body;
+    try {
+        
+        const findUser=await User.findOne({username,password});
+        if(findUser){
+            res.json({success:true,message:'Login Successful'})
+        }
+    } catch (error) {
+        console.error("Error in Login:",error.message);
+        res.json({success:false,message:'Invald Username and Password'})
+    }
+});
+
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on http://localhost:${PORT}`);
--- 06 Capstone/Session 61/frontend/backend/src/App.js
+++ 06 Capstone/Session 61/frontend/backend/src/App.js
@@ -22,43 +22,89 @@ function App() {
     }
   }
 
+  const handleLogin = async () => {
+    try {
+
+      const response = await axios.post('http://localhost:5000/login', { username, password });
+      console.log(response.data);
+      setMessage(response.data.message);
+    } catch (error) {
+      console.log(error.message);
+      setMessage('Error Signing up! Try Again');
+    }
+  }
+
 
   return (
-    <div className='container text-center'>
+    <div className='row'>
+      <div className='col'>
+      <div className='container text-center'>
 
 
+<Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWbCb2mH7L32qlZKWZR-msr8qrHY8Jm5VJdw&usqp=CAU" />
+  <Card.Body>
+    <Card.Title><h1 className='text-primary'>SignUp App</h1></Card.Title>
+    <Card.Text>
+      Please Fill The Form
+      <div className='container'>
+        <div className='row mb-5'>
+          <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className='row mb-5'>
+          <input type='text' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        </div>
+
+      </div>
+    </Card.Text>
+    <div className='container'>
+      <button className='btn btn-success' onClick={handleSignup}>Sign Up</button>
+
+    </div>
+  </Card.Body>
+</Card>
+
+
+
+
+
+
+</div>
+
+      </div>
+      <div className='col'>
       <Card style={{ width: '20rem' }}>
-        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWbCb2mH7L32qlZKWZR-msr8qrHY8Jm5VJdw&usqp=CAU" />
-        <Card.Body>
-          <Card.Title><h1 className='text-primary'>SignUp App</h1></Card.Title>
-          <Card.Text>
-            Please Fill The Form
-            <div className='container'>
-              <div className='row mb-5'>
-                <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-              </div>
-              <div className='row mb-5'>
-                <input type='text' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+  <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWbCb2mH7L32qlZKWZR-msr8qrHY8Jm5VJdw&usqp=CAU" />
+  <Card.Body>
+    <Card.Title><h1 className='text-primary'>Login App</h1></Card.Title>
+    <Card.Text>
+      Please Provide Your Details
+      <div className='container'>
+        <div className='row mb-5'>
+          <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className='row mb-5'>
+          <input type='text' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
-              </div>
+        </div>
 
-            </div>
-          </Card.Text>
-          <div className='container'>
-            <button className='btn btn-success' onClick={handleSignup}>Sign Up</button>
+      </div>
+    </Card.Text>
+    <div className='container'>
+      <button className='btn btn-success' onClick={handleLogin}>Login</button>
 
-          </div>
-        </Card.Body>
-      </Card>
+    </div>
+  </Card.Body>
+</Card>
 
-
-
-
+      </div>
       <div>
 
-        <p className='text-danger'>{message}</p>
-      </div>
+  <p className='text-danger'>{message}</p>
+</div>
     </div>
+    
   );
 }
 
